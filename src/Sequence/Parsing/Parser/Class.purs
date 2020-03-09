c2ed3c7d948797ec8075e7ad6b6904aaccb1b8e8
Joe Gomain Hoyes <gomain@gmail.com>
2020-03-09 15:08:51 +0700

    Parsable instances for String, Array a, List a

--- src/Sequence/Parsing/Parser/Class.purs
+++ src/Sequence/Parsing/Parser/Class.purs
@@ -9,18 +9,22 @@ module Sequence.Parsing.Parser.Class (
   stripPrefix,
   class Update,
   update,
-  class Parsable ) where
+  class Parsable,
+  StringPos,
+  Index ) where
 
 import Control.MonadPlus (guard)
 import Data.Array as A
 import Data.Default (class Default)
+import Data.Foldable (class Foldable, foldl)
+import Data.Foldable as F
 import Data.List (List)
 import Data.List as L
 import Data.Maybe (Maybe, isNothing)
 import Data.Newtype (class Newtype, over, un)
 import Data.String.CodePoints (CodePoint)
 import Data.String.CodePoints as CP
-import Prelude (class Eq, bind, discard, identity, pure, ($), (+), (<<<), (==))
+import Prelude (class Eq, bind, discard, flip, identity, mod, pure, ($), (+), (-), (<<<), (==))
 
 class Represent tee tive | tee -> tive where
   rep :: tee -> tive
@@ -86,7 +90,39 @@ dropMatchingHead_ uncons a as = do
 
 class Update with updatable where
   update :: with -> updatable -> updatable
-  
+
+newtype StringPos = StringPos { line :: Int, column :: Int }
+
+derive instance newtypeStringPos :: Newtype StringPos _
+
+instance defaultStringPos :: Default StringPos where
+  def = StringPos { line: 1, column: 1 }
+
+instance updateCodePointStringPos :: Update CodePoint StringPos where
+  update char (StringPos { line, column }) = StringPos $ case char of
+    c | c `A.elem` [newline, return] -> { line: line + 1, column: 1 }
+      | c == tab                     -> { line,           column: column + 8 - ((column -1) `mod` 8) }
+    _                                -> { line,           column: column + 1 }
+    where newline = CP.codePointFromChar '\n'
+          return  = CP.codePointFromChar '\r'
+          tab     = CP.codePointFromChar '\t'
+
+instance updateStringStringPos :: Update String StringPos where
+  update str pos = foldl (flip update) pos (CP.toCodePointArray str)
+
+newtype Index = Index Int
+
+derive instance newtypeIndexedSequencePos :: Newtype Index _
+
+instance defaultIndex :: Default Index where
+  def = Index 0
+
+instance updateFoldableAIndex :: Foldable f => Update (f a) Index where
+  update f = over Index $ (_ + F.length f)
+else
+instance updateAIndex :: Update a Index where
+  update _ = over Index (_ + 1)
+
 class (Represent seq rep,
        Sequence rep elem,
        StripPrefix seq rep,
@@ -95,3 +131,9 @@ class (Represent seq rep,
        Update seq pos,
        Update elem pos) <=
        Parsable seq elem rep pos | seq -> elem rep pos
+
+instance parsableString :: Parsable String CodePoint (IndexedSequenceRep (Array CodePoint)) StringPos
+
+instance parsableArrayA :: Eq a => Parsable (Array a) a (IndexedSequenceRep (Array a)) Index
+
+instance parsableListA :: Eq a => Parsable (List a) a (List a) Index
