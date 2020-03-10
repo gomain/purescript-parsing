83ba641ca965574934aa60f2d274034bb6c0f3d7
Joe Gomain Hoyes <gomain@gmail.com>
2020-03-10 23:46:59 +0700

    StringParserT

--- src/Sequence/Parsing/Parser/String.purs
+++ src/Sequence/Parsing/Parser/String.purs
@@ -1,92 +1,65 @@
 -- | Primitive parsers for working with an input stream of type `String`.
 
-module Text.Parsing.Parser.String where
+module Sequence.Parsing.Parser.String where
 
 import Prelude hiding (between)
 
-import Control.Monad.State (gets, modify_)
+import Control.Alt ((<|>))
 import Data.Array (many)
+import Data.Enum (fromEnum)
 import Data.Foldable (elem, notElem)
-import Data.Maybe (Maybe(..))
-import Data.Newtype (wrap)
-import Data.String (Pattern, length)
-import Data.String as S
-import Data.String.CodeUnits as SCU
-import Text.Parsing.Parser (ParseState(..), ParserT, fail)
-import Text.Parsing.Parser.Combinators (tryRethrow, (<?>))
-import Text.Parsing.Parser.Pos (updatePosString)
+import Data.String.CodePoints (CodePoint)
+import Data.String.CodePoints as CP
+import Sequence.Parsing.Parser (ParserT, fail)
+import Sequence.Parsing.Parser.Class (IndexedSequenceRep, StringPos)
+import Sequence.Parsing.Parser.Combinators ((<?>))
+import Sequence.Parsing.Parser.Primitives as P
 
--- | This class exists to abstract over streams which support the string-like
--- | operations which this modules needs.
-class StringLike s where
-  drop :: Int -> s -> s
-  indexOf :: Pattern -> s -> Maybe Int
-  null :: s -> Boolean
-  uncons :: s -> Maybe { head :: Char, tail :: s }
-
-instance stringLikeString :: StringLike String where
-  uncons = SCU.uncons
-  drop = S.drop
-  indexOf = S.indexOf
-  null = S.null
+type StringParserT m a = ParserT String (IndexedSequenceRep (Array CodePoint)) StringPos m a
 
 -- | Match end-of-file.
-eof :: forall s m. StringLike s => Monad m => ParserT s m Unit
-eof = do
-  input <- gets \(ParseState input _ _) -> input
-  unless (null input) (fail "Expected EOF")
+eof :: forall m. Monad m => StringParserT m Unit
+eof = P.end <?> "Expected EOF"
 
 -- | Match the specified string.
-string :: forall s m. StringLike s => Monad m => String -> ParserT s m String
-string str = do
-  input <- gets \(ParseState input _ _) -> input
-  case indexOf (wrap str) input of
-    Just 0 -> do
-      modify_ \(ParseState _ position _) ->
-        ParseState (drop (length str) input)
-                   (updatePosString position str)
-                   true
-      pure str
-    _ -> fail ("Expected " <> show str)
+string :: forall m. Monad m => String -> StringParserT m String
+string str = P.string str <?> ("Expected " <> show str)
 
--- | Match any character.
-anyChar :: forall s m. StringLike s => Monad m => ParserT s m Char
-anyChar = do
-  input <- gets \(ParseState input _ _) -> input
-  case uncons input of
-    Nothing -> fail "Unexpected EOF"
-    Just { head, tail } -> do
-      modify_ \(ParseState _ position _) ->
-        ParseState tail
-                   (updatePosString position (SCU.singleton head))
-                   true
-      pure head
+-- | Match any code point.
+anyCodePoint :: forall m. Monad m => StringParserT m CodePoint
+anyCodePoint = P.any <?> "Unexpected EOF"
 
--- | Match a character satisfying the specified predicate.
-satisfy :: forall s m. StringLike s => Monad m => (Char -> Boolean) -> ParserT s m Char
-satisfy f = tryRethrow do
-  c <- anyChar
-  if f c then pure c
-         else fail $ "Character '" <> SCU.singleton c <> "' did not satisfy predicate"
+-- | Match a code point satisfying the specified predicate.
+satisfy :: forall m. Monad m => (CodePoint -> Boolean) -> StringParserT m CodePoint
+satisfy pred = P.when pred
+               <|> failShowCodePoint "does not satisfy predicate"
 
--- | Match the specified character
-char :: forall s m. StringLike s => Monad m => Char -> ParserT s m Char
-char c = satisfy (_ == c) <?> show c
+-- | Match the specified code point
+codePoint :: forall m. Monad m => CodePoint -> StringParserT m CodePoint
+codePoint cp = P.match cp <|> failShowCodePoint ("does not match " <> show cp)
 
--- | Match zero or more whitespace characters.
-whiteSpace :: forall s m. StringLike s => Monad m => ParserT s m String
-whiteSpace = do
-  cs <- many $ satisfy \c -> c == '\n' || c == '\r' || c == ' ' || c == '\t'
-  pure $ SCU.fromCharArray cs
+-- | Match zero or more whitespace.
+whiteSpace :: forall m. Monad m => StringParserT m String
+whiteSpace = CP.fromCodePointArray <$> many (satisfy isWhiteSpace) where
+  isWhiteSpace :: CodePoint -> Boolean
+  isWhiteSpace = flip elem whiteSpaces <<< fromEnum where
+    -- https://en.wikipedia.org/wiki/Whitespace_character
+    whiteSpaces = [9,10,11,12,13,32,133,160,5760,8192,8193,8194,8195,8196,8197,
+                   8198,8199,8200,8201,8202,8232,8233,8239,8287,12288]
 
 -- | Skip whitespace characters.
-skipSpaces :: forall s m. StringLike s => Monad m => ParserT s m Unit
+skipSpaces :: forall m. Monad m => StringParserT m Unit
 skipSpaces = void whiteSpace
 
 -- | Match one of the characters in the array.
-oneOf :: forall s m. StringLike s => Monad m => Array Char -> ParserT s m Char
-oneOf ss = satisfy (flip elem ss) <?> ("one of " <> show ss)
+oneOf :: forall m. Monad m => Array CodePoint -> StringParserT m CodePoint
+oneOf cps = satisfy (flip elem cps)
+            <|> failShowCodePoint ("is not one of " <> show cps)
 
 -- | Match any character not in the array.
-noneOf :: forall s m. StringLike s => Monad m => Array Char -> ParserT s m Char
-noneOf ss = satisfy (flip notElem ss) <?> ("none of " <> show ss)
+noneOf :: forall m. Monad m => Array CodePoint -> StringParserT m CodePoint
+noneOf cps = satisfy (flip notElem cps)
+             <|> failShowCodePoint ("is none of " <> show cps)
+
+failShowCodePoint :: forall m a. Monad m => String -> StringParserT m a
+failShowCodePoint msg = anyCodePoint >>= \cp -> fail $ "'" <> show cp <> "' " <> msg
